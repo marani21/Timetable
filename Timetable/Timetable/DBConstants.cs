2e9fd0a438179e5d622330e03e354bcdbae7eade
Martyna <anielska.martyna@gmail.com>
2016-04-23 11:23:07 +0200

    Nowe klasy ze stałymi. - DBCostants - ViewConstants

---
+++ Timetable/Timetable/DBConstants.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable
+{
+	// Klasa posiadająca stałe z nazwami elementów bazy danych (tabele, kolumny...)
+	class DBConstants
+	{
+		// Tabele
+		public static readonly string CLASSES = "classes";
+		public static readonly string CLASSROOMS = "classrooms";
+		public static readonly string LESSONS = "lessons";
+		public static readonly string LESSONS_PERIODS = "lessons_periods";
+		public static readonly string STUDENTS = "students";
+		public static readonly string SUBJECTS = "subjects";
+		public static readonly string TEACHERS = "teachers";
+		public static readonly string TEACHING = "teaching";
+		public static readonly string WEEKDAYS = "weekdays";
+
+		// Widoki
+		public static readonly string DATABASE_VIEW = "database_view";
+
+		//Kolumny
+		public static readonly string CLASS_ID = "label";
+		public static readonly string CLASSROOM_NUMBER = "number";
+
+		public static readonly string LESSON_LESSON_NUMBER = "lesson_number";
+		public static readonly string LESSON_CLASS = "class";
+		public static readonly string LESSON_SUBJECT = "subject";
+		public static readonly string LESSON_WEEKDAY = "weekday";
+		public static readonly string LESSON_CLASSROOM = "classroom";
+
+		public static readonly string LESSON_PERIOD_NUMBER = "lesson_number";
+		public static readonly string LESSON_PERIOD_START_TIME = "start_time";
+		public static readonly string LESSON_PERIOD_END_TIME = "end_time";
+
+		public static readonly string STUDENT_PESEL = "pesel";
+		public static readonly string STUDENT_NAME = "name";
+		public static readonly string STUDENT_SURNAME = "surname";
+		public static readonly string STUDENT_CLASS_ID = "class";
+
+		public static readonly string SUBJECT_ID = "id";
+		public static readonly string SUBJECT_NAME = "name";
+
+		public static readonly string TEACHER_PESEL = "pesel";
+		public static readonly string TEACHER_NAME = "name";
+		public static readonly string TEACHER_SURNAME = "surname";
+
+		public static readonly string TEACHING_CLASS_ID = "class";
+		public static readonly string TEACHING_TEACHER_ID = "teacher";
+		public static readonly string TEACHING_SUBJECT_ID = "subject";
+		public static readonly string TEACHING_AMOUNT = "amount";
+
+		public static readonly string WEEKDAY_ID = "id";
+		public static readonly string WEEKDAY_NAME = "name";
+	}
+}
---
+++ Timetable/Timetable/ViewConstants.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable
+{
+	// Klasa posiadająca stałe, które wyświetlamy w widoku
+	class ViewConstants
+	{
+		public static readonly string CLASS = "klasa";
+		public static readonly string CLASSROOM = "sala";
+		public static readonly string NAME = "imię";
+		public static readonly string PERSONAL_IDENTITY_NUMBER = "pesel";
+		public static readonly string STUDENT = "uczeń";
+		public static readonly string SUBJECT = "przedmiot";
+		public static readonly string SURNAME = "nazwisko";
+		public static readonly string TEACHER = "nauczyciel";
+
+		public static readonly string SCHEDULE_FOR_CLASS = "Plan dla klasy: ";
+		public static readonly string SUBJECT_FOR_CLASS = "Przedmiot dla klasy: ";
+		public static readonly string SCHEDULES_MANAGER = "Manager planów lekcji";
+		public static readonly string ADD_DATA_TO_DATABASE = "Dodaj dane do bazy";
+		public static readonly string VIEW_SCHEDULES = "Przeglądaj plany";
+		public static readonly string CREATE_SCHEDULE = "Ułóż plan";
+
+		// Akcje
+		public static readonly string INSTER = "Wstaw";
+		public static readonly string DELETE = "Usuń";
+		public static readonly string ADD = "Dodaj";
+		public static readonly string EDIT = "Edytuj";
+		public static readonly string CANCEL = "Anuluj";
+		public static readonly string OK = "OK";
+	
+		// Komunikaty
+		public static readonly string CELL_NOT_CHOSEN = "Nie wybrano żadnej komórki";
+		public static readonly string CLASSROOM_BUSY = "Sala jest zajęta przez ";
+		public static readonly string SAVE_CHANGES_QUESTION = "Czy chcesz zapisać wszystkie dokonane zmiany?";
+		public static readonly string TEACHER_BUSY = "Nauczyciel jest zajęty ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
